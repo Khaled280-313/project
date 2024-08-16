e3ca6e701e056f4b1d76f5b495e58f6867bc481b
Khaled <almaysari@gmail.com>
2024-08-16 21:14:55 +0300

    Add Container for SingleChildScrollView in Row

--- lib/main.dart
+++ lib/main.dart
@@ -16,29 +16,31 @@ class MyApp extends StatelessWidget {
             title: const Text("Kaled"),
             backgroundColor: const Color.fromARGB(184, 128, 64, 207),
           ),
-          body: SingleChildScrollView(
-            
-              child: Column(
-            children: [
-              Container(
-                width: 200,
-                height: 200,
-                margin: const EdgeInsets.all(30),
-                padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
-                decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 21, 223, 162),
-                    border: Border.all(color: Colors.black, width: 3),
-                    borderRadius: BorderRadius.circular(50),
-                    boxShadow: const [
-                      BoxShadow(
-                          color: Color.fromARGB(96, 163, 195, 8),
-                          offset: Offset(10, 10),
-                          blurRadius: 10)
-                    ]),
-                child: Image.asset("assets/images/breakfast.PNG"),
-              )
-            ],
-          ))),
+          body: Container(
+            child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 200,
+                      height: 200,
+                      margin: const EdgeInsets.all(30),
+                      padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
+                      decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 21, 223, 162),
+                          border: Border.all(color: Colors.black, width: 3),
+                          borderRadius: BorderRadius.circular(50),
+                          boxShadow: const [
+                            BoxShadow(
+                                color: Color.fromARGB(96, 163, 195, 8),
+                                offset: Offset(10, 10),
+                                blurRadius: 10)
+                          ]),
+                      child: Image.asset("assets/images/breakfast.PNG"),
+                    )
+                  ],
+                )),
+          )),
     );
   }
 }
