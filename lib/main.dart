9888c552e0390076ffc8393b6e8e1b97fadbc45f
Khaled <almaysari@gmail.com>
2024-08-16 21:17:09 +0300

    Add ListView in Container

--- lib/main.dart
+++ lib/main.dart
@@ -17,29 +17,29 @@ class MyApp extends StatelessWidget {
             backgroundColor: const Color.fromARGB(184, 128, 64, 207),
           ),
           body: Container(
-            child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    Container(
-                      width: 200,
-                      height: 200,
-                      margin: const EdgeInsets.all(30),
-                      padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
-                      decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 21, 223, 162),
-                          border: Border.all(color: Colors.black, width: 3),
-                          borderRadius: BorderRadius.circular(50),
-                          boxShadow: const [
-                            BoxShadow(
-                                color: Color.fromARGB(96, 163, 195, 8),
-                                offset: Offset(10, 10),
-                                blurRadius: 10)
-                          ]),
-                      child: Image.asset("assets/images/breakfast.PNG"),
-                    )
-                  ],
-                )),
+            child: ListView(scrollDirection: Axis.horizontal, children: [
+              Row(
+                children: [
+                  Container(
+                    width: 200,
+                    height: 200,
+                    margin: const EdgeInsets.all(30),
+                    padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
+                    decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 21, 223, 162),
+                        border: Border.all(color: Colors.black, width: 3),
+                        borderRadius: BorderRadius.circular(50),
+                        boxShadow: const [
+                          BoxShadow(
+                              color: Color.fromARGB(96, 163, 195, 8),
+                              offset: Offset(10, 10),
+                              blurRadius: 10)
+                        ]),
+                    child: Image.asset("assets/images/breakfast.PNG"),
+                  )
+                ],
+              )
+            ]),
           )),
     );
   }
