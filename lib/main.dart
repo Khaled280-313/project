74132dbe61f63669bbd65ccfd22a5d1280dea7b4
Khaled <almaysari@gmail.com>
2024-08-16 20:55:49 +0300

    add  Container

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,28 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.yellowAccent,
-        appBar: AppBar(
-          title: const Text("Kaled"),
-          backgroundColor: const Color.fromARGB(184, 128, 64, 207),
-        ),
-        body: Center(
-          child: Image.asset("assets/images/breakfast.PNG"),
-        )
-      ),
+          backgroundColor: Colors.yellowAccent,
+          appBar: AppBar(
+            title: const Text("Kaled"),
+            backgroundColor: const Color.fromARGB(184, 128, 64, 207),
+          ),
+          body: Container(
+            width: 200,
+            height: 200,
+            margin: const EdgeInsets.all(30),
+            padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
+            decoration:  BoxDecoration(
+              color:  const Color.fromARGB(255, 21, 223, 162),
+              border:  Border.all( color: Colors.black ,width: 3),
+              borderRadius: BorderRadius.circular(50),
+              boxShadow: const [BoxShadow(
+                color: Color.fromARGB(96, 163, 195, 8),
+                offset: Offset(10, 10),
+                blurRadius: 10
+              )]
+            ),
+            child: Image.asset("assets/images/breakfast.PNG"),
+          )),
     );
   }
 }
