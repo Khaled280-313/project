cee93c28b55eca5176907c9c9dd4cda51d66d2bb
Khaled <almaysari@gmail.com>
2024-08-18 19:22:30 +0300

    Example

--- lib/main.dart
+++ lib/main.dart
@@ -11,36 +11,147 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.yellowAccent,
-          appBar: AppBar(
-            title: const Text("Kaled"),
-            backgroundColor: const Color.fromARGB(184, 128, 64, 207),
+        appBar: AppBar(
+          title: const Text("Example"),
+          backgroundColor: const Color.fromARGB(255, 7, 137, 224),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Container(
+                width: 500,
+                margin: const EdgeInsets.fromLTRB(6, 7, 6, 5),
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 190, 222, 249),
+                    border: Border.all(color: Colors.black, width: 1.5)),
+                child: const Text(
+                  "Khaled Mohammed",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                width: 500,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                margin: const EdgeInsets.fromLTRB(6, 5, 6, 6),
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 190, 222, 249),
+                    border: Border.all(color: Colors.black, width: 1.5)),
+                child: const Text(
+                  "Khaled Mohammed nlnlnf aswbkthbw  rj fj w k ruwebrhht eeee rty   rr tew ro7rtyuiu sugb wsxfwhbkb wsibfw sjb jwf  jdv  wgdv davd nhv okbe f cgbj  dh af fhf fe bef fkem khbwdboyww n  w;ndgdf hfe s v",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                width: 500,
+                height: 40,
+                margin: const EdgeInsets.fromLTRB(6, 7, 6, 5),
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 190, 222, 249),
+                    border: Border.all(color: Colors.black, width: 1.5)),
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.star_outlined,
+                          size: 20,
+                        ),
+                        Icon(
+                          Icons.star_outlined,
+                          size: 20,
+                        ),
+                        Icon(
+                          Icons.star_outlined,
+                          size: 20,
+                        ),
+                        Icon(
+                          Icons.star_outlined,
+                          size: 20,
+                        ),
+                        Icon(
+                          Icons.star_outlined,
+                          size: 20,
+                        ),
+                      ],
+                    ),
+                    Text("170 Reviews")
+                  ],
+                ),
+              ),
+              Container(
+                  width: 500,
+                  margin: const EdgeInsets.fromLTRB(6, 7, 6, 5),
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 190, 222, 249),
+                      border: Border.all(color: Colors.black, width: 1.5)),
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const Spacer(),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          const Icon(
+                            Icons.kitchen,
+                            color: Colors.green,
+                            size: 37,
+                          ),
+                          const Text("prep:",
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Container(
+                            height: 10,
+                          ),
+                          const Text("25 min"),
+                        ],
+                      ),
+                      const Spacer(),
+                      Column(
+                        children: [
+                          const Icon(
+                            Icons.alarm,
+                            color: Colors.green,
+                            size: 37,
+                          ),
+                          const Text("Cook:",
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Container(
+                            height: 10,
+                          ),
+                          const Text("1h"),
+                        ],
+                      ),
+                      const Spacer(),
+                      Column(
+                        children: [
+                          const Icon(
+                            Icons.restaurant,
+                            color: Colors.green,
+                            size: 37,
+                          ),
+                          const Text(
+                            "Feeds",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          Container(
+                            height: 10,
+                          ),
+                          const Text("30 min"),
+                        ],
+                      ),
+                      const Spacer(),
+                    ],
+                  ))
+            ],
           ),
-          body: Container(
-            child: ListView(scrollDirection: Axis.horizontal, children: [
-              Row(
-                children: [
-                  Container(
-                    width: 200,
-                    height: 200,
-                    margin: const EdgeInsets.all(30),
-                    padding: const EdgeInsets.fromLTRB(30, 30, 30, 30),
-                    decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 21, 223, 162),
-                        border: Border.all(color: Colors.black, width: 3),
-                        borderRadius: BorderRadius.circular(50),
-                        boxShadow: const [
-                          BoxShadow(
-                              color: Color.fromARGB(96, 163, 195, 8),
-                              offset: Offset(10, 10),
-                              blurRadius: 10)
-                        ]),
-                    child: Image.asset("assets/images/breakfast.PNG"),
-                  )
-                ],
-              )
-            ]),
-          )),
+        ),
+      ),
     );
   }
 }
